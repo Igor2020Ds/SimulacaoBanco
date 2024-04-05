553d7796221fdef4bd8d169f005319b6f8676220
IGOR <IGOR@IGOR>
2024-04-05 17:39:09 -0300

    Adicionar arquivos de projeto.

---
+++ SimulacaoBanco/InterfaceUI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+using System.Globalization;
+using Program;
+
+
+
+namespace IntefaceUsuario 
+{
+
+    class InterfaceUI
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.WriteLine("Informe o numero da conta");
+            int numeroConta= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Titular da conta");
+            string  titularConta=(Console.ReadLine());  
+
+            Console.WriteLine("Haverá deposito inicial?");
+            char respDepositoIni = char.Parse(Console.ReadLine());
+
+            if (respDepositoIni == 's' || respDepositoIni =='S')
+            {
+                Console.WriteLine("Informe o valor do depósito inicial");
+                double depositoInicialConta = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numeroConta,titularConta,depositoInicialConta); // chamando o construtor (instanciando)
+
+            }
+
+            else {
+                conta = new ContaBancaria(numeroConta, titularConta);
+            }
+
+
+            Console.WriteLine("Dados da Conta : " + conta);
+
+            Console.WriteLine("Digite 1 para realizar DEPÓSITO");
+            int codigo = int.Parse(Console.ReadLine());
+            
+            if (codigo == 1)
+
+            {
+
+                Console.WriteLine("Infome o valor do DEPOSITO");
+                double ValorDepositoInicial = double.Parse(Console.ReadLine());
+
+            }
+
+            else if(codigo == 2) {
+
+                Console.WriteLine("Informe o valor do SAQUE");
+            }
+
+            else
+            {
+                Console.WriteLine("Còdigo incorreto, falha na Operação");
+            }
+
+
+
+
+
+        }
+
+        
+
+        
+    }
+}
---
+++ SimulacaoBanco/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+
+
+
+namespace Program
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; set; }
+        public string TitularConta { get; set; }
+        public double SaldoConta { get; set; }
+
+        public double ValorDepositoInicial { get; set; }
+
+        public double ValorDeposito { get; set; }
+
+        public double ValorSaque { get; set; }
+
+        public int Codigo { get; set; }
+
+        // construtor com 2 argumentos
+        public ContaBancaria(int numeroConta, string titularConta)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+        }
+
+        public ContaBancaria(int numeroConta, string titularConta, double saldoConta) : this(numeroConta, titularConta) // this reaproveita o contrutor
+        {
+            SaldoConta = saldoConta;
+        }
+
+
+        public void Deposito(double quantia)
+        {
+            SaldoConta += quantia;
+        }
+
+        public override string ToString() // prepara recebimento de conteúdo textual
+
+            {
+            return  " Conta "
+        + NumeroConta
+        + "  Titular :   "
+        + TitularConta
+        + "Saldo $ "
+        + SaldoConta.ToString("F2", CultureInfo.InvariantCulture);
+            }
+
+        
+
+
+    }
+}
